7ad480920982a8a1ac8dcbe06390c0d2c512d844
eroha <oyeroshkin30@gmail.com>
2024-06-04 15:08:58 +0300

    Initial Commit

---
+++ Test task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test task
+//
+//  Created by oleh yeroshkin on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
